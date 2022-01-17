4515a36a7d5554e50ca56200b5f514b9550fcd5d
mluis210393 <mluis_21@hotmail.com>
2022-01-17 11:23:44 -0600

    Desafios

---
+++ script.js
@@ -0,0 +1,29 @@
+function arreglos(){
+
+var carreritas =["Lucía", "Roberto", "María", "Jesús", "Andrea", "Jóse"];
+
+
+carreritas
+
+
+let nuevacarreritas = carreritas.pop();
+
+
+console.log(carreritas);
+
+
+let nuevosParticipantes = carreritas.unshift('Cristobal','Fernanda','Armando');
+
+
+console.log(carreritas);
+
+let nuevoParticipante = carreritas.push('Federico');
+
+console.log(carreritas);
+
+
+}
+
+
+
+arreglos();
